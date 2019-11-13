801839cf9aba6e74ac700987e39cf78fbf79415f
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-11-13 18:00:58 -0500

    MVP has been reached

--- users/users-router.js
+++ users/users-router.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const Users = require("./users-model.js");
 const restricted = require("../auth/restricted-middleware.js");
 
-router.get("/", restricted, checkRole(["accounting", "admin"]), (req, res) => {
+router.get("/", restricted, checkDepartment(["department", "accounting"]), (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
@@ -13,9 +13,9 @@ router.get("/", restricted, checkRole(["accounting", "admin"]), (req, res) => {
 
 module.exports = router;
 
-function checkRole(roles) {
+function checkDepartment(departments) {
   return function(req, res, next) {
-    if (roles.includes(req.decodedJwt.role)) {
+    if (departments.includes(req.decodedJwt.department)) {
       next();
     } else {
       res.status(403).json({ message: "Can't touch this!" });
